fdeffdde56023fc7b52ccf917843fd15e371f144
slrnd <wlfhrtd@gmail.com>
2022-08-02 18:43:37 +0300

    add singleton v2

---
+++ Behavioral/02. Singleton/Singleton/v2_Locking/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Singleton.v2_Locking
+{
+    public sealed class Singleton
+    {
+        private static Singleton? instance = null;
+
+        private static readonly object padlock = new object();
+
+        // private parameterless; if need parameters look for Factory
+        private Singleton()
+        {
+            // can only be instantiated from within class
+            Logger.Log("Constructor invoked.");
+        }
+
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Logger.Log("Instance called.");
+                // heavy performance hit since applying lock for every reference without check (for null) do we really need lock
+                lock (padlock)
+                {
+                    return instance ??= new Singleton();
+                }
+            }
+        }
+    }
+}
---
+++ Behavioral/02. Singleton/Singleton/v2_Locking/SingletonInstance.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace Singleton.v2_Locking
+{
+    public class SingletonInstance
+    {
+        private readonly ITestOutputHelper output;
+
+
+        public SingletonInstance(ITestOutputHelper helper)
+        {
+            output = helper;
+            SingletonTestHelpers.Reset(typeof(Singleton));
+            Logger.Clear();
+        }
+
+
+        [Fact]
+        public void ReturnsNonNullSingletonInstance()
+        {
+            Assert.Null(SingletonTestHelpers.GetPrivateStaticInstance<Singleton>());
+
+            var result = Singleton.Instance;
+
+            Assert.NotNull(result);
+            Assert.IsType<Singleton>(result);
+
+            Logger.Output().ToList().ForEach(h => output.WriteLine(h));
+        }
+
+        [Fact]
+        public void OnlyCallsConstructorOnceGivenThreeInstanceCalls()
+        {
+            Assert.Null(SingletonTestHelpers.GetPrivateStaticInstance<Singleton>());
+
+            // configure logger to slow down the creation longer than pauses below
+            Logger.DelayMilliseconds = 10;
+
+            var one = Singleton.Instance;
+            Thread.Sleep(1);
+            var two = Singleton.Instance;
+            Thread.Sleep(1);
+            var three = Singleton.Instance;
+
+            var log = Logger.Output();
+            Assert.Equal(1, log.Count(log => log.Contains("Constructor")));
+            Assert.Equal(3, log.Count(log => log.Contains("Instance")));
+
+            Logger.Output().ToList().ForEach(h => output.WriteLine(h));
+        }
+
+        [Fact]
+        public void CallsConstructorMultipleTimesGivenThreeParallelInstanceCalls()
+        {
+            Assert.Null(SingletonTestHelpers.GetPrivateStaticInstance<Singleton>());
+
+            // configure logger to slow down the creation long enough to cause problems
+            Logger.DelayMilliseconds = 50;
+
+            var strings = new List<string>() { "one", "two", "three" };
+            var instances = new List<Singleton>();
+            var options = new ParallelOptions() { MaxDegreeOfParallelism = 3 }; // 3 threads
+
+            Parallel.ForEach(strings, options, instance =>
+            {
+                instances.Add(Singleton.Instance);
+            });
+
+            var log = Logger.Output();
+
+            try
+            {
+                Assert.Equal(1, log.Count(log => log.Contains("Constructor")));
+                Assert.Equal(3, log.Count(log => log.Contains("Instance")));
+            }
+            finally
+            {
+                Logger.Output().ToList().ForEach(h => output.WriteLine(h));
+            }
+        }
+    }
+}
