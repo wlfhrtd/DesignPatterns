99bd13b341f8f46c83ac289b27612813f1c03b98
slrnd <wlfhrtd@gmail.com>
2022-08-02 18:56:16 +0300

    add singleton v3

---
+++ Behavioral/02. Singleton/Singleton/v3_BetterLocking/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Singleton.v3_BetterLocking
+{
+    public sealed class Singleton
+    {
+        private static Singleton? instance = null;
+
+        private static readonly object padlock = new object();
+
+        // private parameterless; if need parameters look for Factory
+        private Singleton()
+        {
+            // can only be instantiated from within class
+            Logger.Log("Constructor invoked.");
+        }
+
+
+        public static Singleton Instance
+        {
+            get
+            {
+                Logger.Log("Instance called.");
+                // acquire lock only when needed - if only instance is null ~~ better performance
+                if (instance == null)
+                {
+                    lock(padlock)
+                    {
+                        instance ??= new Singleton();
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+}
